edf7acc7b664de549255b69d1875ba32d7d04927
MK13-11 <montrevilkensly1311@gmail.com>
2023-08-24 16:34:55 -0400

    Connection de la base de données avec TypeOrm MySql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      username: 'root',
+      password: 'Herntz3.0',
+      database: 'bookstore',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
