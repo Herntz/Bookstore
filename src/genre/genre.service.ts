03920f97cae179aa889a32073cbdd779d9b619f9
Rosefloredine <rosefloredineflora@gmail.com>
2023-08-29 23:50:06 +0100

    .

--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGenreDto } from './dto/create-genre.dto';
 import { UpdateGenreDto } from './dto/update-genre.dto';
+import { GenreEntity } from './entities/genre.entity';
+import { Repository } from 'typeorm'
+import { CurrentUser } from 'src/utility/decorators/current-user.decorator';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class GenreService {
-  create(createGenreDto: CreateGenreDto) {
-    return 'This action adds a new genre';
+  constructor(@InjectRepository(GenreEntity)private readonly 
+  genreRepository:Repository<GenreEntity>){}
+
+ async create(createGenreDto: CreateGenreDto, @CurrentUser() currentUser:UserEntity):Promise<GenreEntity> {
+    const genre= await this.genreRepository.create(createGenreDto);
+    genre.addBy=currentUser;
+    return await this.genreRepository.save(genre);
+    
   }
 
   findAll() {
@@ -24,3 +34,7 @@ export class GenreService {
     return `This action removes a #${id} genre`;
   }
 }
+function InjectRepository(GenreEntity: any): (target: typeof GenreService, propertyKey: undefined, parameterIndex: 0) => void {
+  throw new Error('Function not implemented.');
+}
+
