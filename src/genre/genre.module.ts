4743a49c522fff4db4708749b3145ef677d26aef
Rosefloredine <rosefloredineflora@gmail.com>
2023-08-30 18:08:30 +0100

    .

--- src/genre/genre.module.ts
+++ src/genre/genre.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GenreService } from './genre.service';
 import { GenreController } from './genre.controller';
+import { GenreEntity } from './entities/genre.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([GenreEntity])],
   controllers: [GenreController],
   providers: [GenreService],
 })
